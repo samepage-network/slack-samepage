6f327013d50304561398a19d4ff717365868a6c2
David Vargas <dvargas92495@gmail.com>
2023-04-17 18:33:41 -0400

    Slack oauth set up

--- api/oauth/post.ts
+++ api/oauth/post.ts
@@ -2,26 +2,26 @@ import createAPIGatewayHandler from "samepage/backend/createAPIGatewayProxyHandl
 import { zOauthRequest, zOauthResponse } from "samepage/internal/types";
 import { z } from "zod";
 import axios from "axios";
+import qs from "querystring";
 
 const logic = async (
   args: z.infer<typeof zOauthRequest>
 ): Promise<z.infer<typeof zOauthResponse>> => {
   const { data } = await axios
-    .post<{ access_token: string }>(
-      // TODO - REPLACE THIS WITH THE URL OF THE APP's OAUTH PROVIDER
-      `https://api.samepage.network/oauth`,
-      {
+    .post<{ access_token: string; team: { name: string } }>(
+      "https://slack.com/api/oauth.v2.access",
+      qs.stringify({
         code: args.code,
         redirect_uri:
           process.env.NODE_ENV === "production"
-            ? "https://samepage.network/oauth/github"
-            : "https://samepage.ngrok.io/oauth/github",
+            ? "https://samepage.network/oauth/slack"
+            : "https://samepage.ngrok.io/oauth/slack",
         client_id: process.env.OAUTH_CLIENT_ID,
         client_secret: process.env.OAUTH_CLIENT_SECRET,
-      },
+      }),
       {
         headers: {
-          Accept: "application/json",
+          Accept: "application/x-www-form-urlencoded",
         },
       }
     )
@@ -30,12 +30,10 @@ const logic = async (
         new Error(`Failed to get access token: ${e.response.data}`)
       )
     );
-  const { access_token } = data;
-  // @ts-ignore - TODO - REPLACE THIS WITH THE CODE TO GET THE WORKSPACE NAME
-  const workspace = await getWorkspaceNameUsingAccessToken(access_token);
+  const { access_token, team } = data;
   return {
     accessToken: access_token,
-    workspace,
+    workspace: team.name,
   };
 };
 
